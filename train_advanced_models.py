d03ca6232e47cd7fe3df162701dadca6c2b6f2a2
Rushikesh Hippargekar <137420058+Rushstatic@users.noreply.github.com>
2025-08-26 12:07:30 +0530

    Update train_advanced_models.py

--- train_advanced_models.py
+++ train_advanced_models.py
@@ -1,48 +1,46 @@
-# train_advanced_models.py (with Feature Importance)
+# train_advanced_models.py
 import pandas as pd
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from xgboost import XGBClassifier
 
-print("--- Script Started ---")
+print("  Script Started  ")
 
-# --- 1. Load the FINAL NLP-Enriched Data ---
+# Load
 DATASET_PATH = 'nlp_enriched_data.csv'
 print(f"Loading final dataset from '{DATASET_PATH}'...")
 df = pd.read_csv(DATASET_PATH)
 
-# --- 2. Define Features (X) and Target (y) ---
+# Define features
 features = df.columns.drop('target')
 target = 'target'
 
 X = df[features]
 y = df[target]
 
-# --- 3. Split Data and Train Model ---
+# Split model
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 print(f"Training XGBoost Model on {len(X_train)} samples...")
 xgb_model = XGBClassifier(use_label_encoder=False, eval_metric='logloss', random_state=42)
 xgb_model.fit(X_train, y_train)
 
-# --- 4. Evaluate ---
+# Evaluate
 print("\n--- XGBoost NLP Model Classification Report ---")
 y_pred_xgb = xgb_model.predict(X_test)
 print(classification_report(y_test, y_pred_xgb, target_names=['Real', 'Fake']))
 
-# --- 5. SHOW THE NLP RESULTS (FEATURE IMPORTANCE) ---
+# Nlp feature words as well
 print("\n--- Top 15 Most Important Features ---")
-# Create a DataFrame of features and their importance scores
+
 feature_importance = pd.DataFrame({
     'feature': features,
     'importance': xgb_model.feature_importances_
 }).sort_values('importance', ascending=False)
 
-# Print the top 15
+# Can change no for more or less features
 print(feature_importance.head(15))
 
-
-# --- 6. Save the Final Model ---
 print("\nSaving the final NLP model...")
 joblib.dump(xgb_model, 'final_nlp_model.joblib')
 print("Model saved as 'final_nlp_model.joblib'.")
